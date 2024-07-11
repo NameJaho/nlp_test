158ffdaff748af04c8f3c1ec2226a4f022173df2
liujeru <jeru.token@gmail.com>
2024-07-11 14:45:42 +0800

    add synonyms

--- embedding/word2vec/synonyms.py
+++ embedding/word2vec/synonyms.py
@@ -5,16 +5,16 @@ import os
 from gensim.models import KeyedVectors
 
 root_path = utils.get_root_path()
-wechat_path = os.path.join(root_path, 'models/vector', 'word2vec_wx')
+wechat_path = os.path.join(root_path, 'models/vector', 'word2vec_baike')
 print(wechat_path)
 
 #model = KeyedVectors.load_word2vec_format(wechat_path)
 model = gensim.models.Word2Vec.load(wechat_path)
 
 # find most similar words
-testwords = ['服务', '出海', '价', '价格', '资源', '动销', '供应链', '生态', '运营', '流量', '平台', '消费者']
+testwords = '跨境', '交易', '价格', '资源', '支付', '供应链', '生态', '运营', '流量', '平台', '消费者'
 for i in range(len(testwords)):
-    res = model.wv.most_similar(testwords[i], topn=5)
+    res = model.wv.most_similar(testwords[i], topn=20)
     print(testwords[i])
     print(res)
 
